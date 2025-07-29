6c808ee53c1c68f14a4293b2805946564c38739b
Young Seo Kim <youngseokim7@gmail.com>
2025-07-29 20:15:37 +0900

    initial attempt at UserRepository

--- src/main/java/com/JCC/LeAtcoderAPI/repositories/TaskRepository.java
+++ src/main/java/com/JCC/LeAtcoderAPI/repositories/TaskRepository.java
@@ -1,4 +1,22 @@
 package com.JCC.LeAtcoderAPI.repositories;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.springframework.stereotype.Repository;
+import static com.mongodb.client.model.Filters.eq;
+
+@Repository
 public class TaskRepository {
+    private MongoCollection collection;
+
+    public TaskRepository(DatabaseClient dbClient) {
+        this.collection = dbClient.getDb().getCollection("tasks");
+    }
+
+    public Document getTaskContent(String questionId) {
+        Bson filter  = eq("id", questionId);
+        return (Document) this.collection.find(filter).first();
+    }
 }
