f3b1e5c072f4cb6a8800517ab9ec69d81a1623b5
Young Seo Kim <youngseokim7@gmail.com>
2025-07-31 22:55:00 +0900

    create data models for Tasks

---
+++ src/main/java/com/JCC/LeAtcoderAPI/Model/Task.java
@@ -0,0 +1,20 @@
+package com.JCC.LeAtcoderAPI.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "tasks")
+public record Task(
+        @Id
+        String _id,
+        String url,
+        String taskName,
+        String score,
+        String statement,
+        String constraints,
+        String input,
+        String output,
+        List<Sample> samples
+){}
