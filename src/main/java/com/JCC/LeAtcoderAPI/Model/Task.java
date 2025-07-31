7cdc34f0c4071b93813a9d1edc39e56992e6343d
Young Seo Kim <youngseokim7@gmail.com>
2025-07-31 23:10:10 +0900

    change score to int (update database as well)

--- src/main/java/com/JCC/LeAtcoderAPI/Model/Task.java
+++ src/main/java/com/JCC/LeAtcoderAPI/Model/Task.java
@@ -11,7 +11,7 @@ public record Task(
         String _id,
         String url,
         String taskName,
-        String score,
+        int score,
         String statement,
         String constraints,
         String input,
