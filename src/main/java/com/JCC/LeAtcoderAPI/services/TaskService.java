15fc6e11c25dc6e47e10654d33448419b7c78b92
Young Seo Kim <youngseokim7@gmail.com>
2025-08-01 16:07:49 +0900

    complete initial implementatino of Task Service

--- src/main/java/com/JCC/LeAtcoderAPI/services/TaskService.java
+++ src/main/java/com/JCC/LeAtcoderAPI/services/TaskService.java
@@ -1,8 +1,14 @@
 package com.JCC.LeAtcoderAPI.services;
 
+import com.JCC.LeAtcoderAPI.Model.ServiceObjects.DifficultyObject;
 import com.JCC.LeAtcoderAPI.Model.Task.Task;
 import com.JCC.LeAtcoderAPI.repositories.TaskRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -13,5 +19,20 @@ public class TaskService {
 
     public Task getTaskContent(String taskId) {
         return this.taskRepository.findByTaskId(taskId);
+    }
+
+    public DifficultyObject getTotalDifficulties() {
+        int beginner = this.taskRepository.countByScoreBetween(0, 200);
+        int easy = this.taskRepository.countByScoreBetween(201, 300);
+        int medium = this.taskRepository.countByScoreBetween(301, 450);
+        int hard = this.taskRepository.countByScoreBetween(451, 650);
+        int expert = this.taskRepository.countByScoreBetween(651, 1500);
+        return new DifficultyObject(beginner, easy, medium, hard, expert);
+    }
+
+    public List<Task> getTaskList(int index, int min, int max) {
+        Pageable pageable = PageRequest.of(index, 50);
+        Page<Task> taskPage = this.taskRepository.findByScoreBetween(min, max, pageable);
+        return taskPage.getContent();
     }
 }
