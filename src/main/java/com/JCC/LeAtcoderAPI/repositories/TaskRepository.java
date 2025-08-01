e0fd8b2e705bbc0f74fdaa27e42e5a434d599d0f
Young Seo Kim <youngseokim7@gmail.com>
2025-08-01 16:07:38 +0900

    update Pageable improt to match data improt

--- src/main/java/com/JCC/LeAtcoderAPI/repositories/TaskRepository.java
+++ src/main/java/com/JCC/LeAtcoderAPI/repositories/TaskRepository.java
@@ -2,11 +2,11 @@ package com.JCC.LeAtcoderAPI.repositories;
 
 import com.JCC.LeAtcoderAPI.Model.Task.Task;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 
 @Repository
 public interface TaskRepository extends MongoRepository<Task, String> {
