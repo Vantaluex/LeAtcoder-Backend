f45e8ddc9af9a5d9423983b88908f69318147207
Young Seo Kim <youngseokim7@gmail.com>
2025-08-04 22:00:09 +0900

    create Oauth2SucessHandler

---
+++ src/main/java/com/JCC/LeAtcoderAPI/security/Oauth2SuccessHandler.java
@@ -0,0 +1,15 @@
+package com.JCC.LeAtcoderAPI.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+
+import java.io.IOException;
+
+public class Oauth2SuccessHandler implements org.springframework.security.web.authentication.AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
+}
