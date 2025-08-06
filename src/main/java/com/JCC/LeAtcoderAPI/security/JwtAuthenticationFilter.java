cddf8d3f81cb4b585cbe0b5b2b6a7a13816e6fdd
Young Seo Kim <youngseokim7@gmail.com>
2025-08-06 17:38:31 +0900

    create jwt auth filter

--- src/main/java/com/JCC/LeAtcoderAPI/security/JwtAuthenticationFilter.java
+++ src/main/java/com/JCC/LeAtcoderAPI/security/JwtAuthenticationFilter.java
@@ -1,6 +1,43 @@
 package com.JCC.LeAtcoderAPI.security;
 
+import com.JCC.LeAtcoderAPI.Model.User.User;
+import com.JCC.LeAtcoderAPI.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || authHeader.startsWith("Bearer ")) return;
+
+        String token = authHeader.substring(7);
+        String userId = jwtService.extractToken(token);
+        if (userId == null) return;
+
+        User user = userService.getAllUserInfo(userId);
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        filterChain.doFilter(request, response);
+    }
 }
