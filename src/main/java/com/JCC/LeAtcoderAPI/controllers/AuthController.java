9d4fbae4c2adc62360c75d9b30b427c3d41b4f41
Young Seo Kim <youngseokim7@gmail.com>
2025-08-10 02:22:36 +0900

    mark as restcontroller instead of just contorller

--- src/main/java/com/JCC/LeAtcoderAPI/controllers/AuthController.java
+++ src/main/java/com/JCC/LeAtcoderAPI/controllers/AuthController.java
@@ -11,13 +11,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
