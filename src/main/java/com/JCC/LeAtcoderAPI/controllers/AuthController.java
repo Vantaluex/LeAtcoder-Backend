7a6ba126efeb0d6989b4efc9870d376723893827
Young Seo Kim <youngseokim7@gmail.com>
2025-08-10 02:21:06 +0900

    fix minor bugs and implement refresh endpoint

--- src/main/java/com/JCC/LeAtcoderAPI/controllers/AuthController.java
+++ src/main/java/com/JCC/LeAtcoderAPI/controllers/AuthController.java
@@ -1,17 +1,19 @@
 package com.JCC.LeAtcoderAPI.controllers;
 
-import com.JCC.LeAtcoderAPI.Model.ControllerObjects.CompleteBody;
+import com.JCC.LeAtcoderAPI.Model.ControllerObjects.AuthResponseBody;
 import com.JCC.LeAtcoderAPI.Model.ServiceObjects.Limits;
 import com.JCC.LeAtcoderAPI.Model.User.User;
 import com.JCC.LeAtcoderAPI.security.JwtService;
 import com.JCC.LeAtcoderAPI.services.UserService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,12 +27,14 @@ public class AuthController {
     JwtService jwtService;
 
     private Cookie createTokenCookie(String token) {
-        Cookie cookie = new Cookie("jwt-token", token);
+        Cookie cookie = new Cookie("refresh-token", token);
         cookie.setMaxAge(Limits.REFRESH_EXPIRY_IN_SECONDS);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         return cookie;
     }
+
+    // keep in mind default oauth endpoint in spring is /oauth2/authorization/google
 
     @GetMapping("/complete")
     public ResponseEntity<?> completeLogin(
@@ -47,6 +51,33 @@ public class AuthController {
         response.addCookie(cookie);
 
         String accessToken = jwtService.createTokenBySubAndExpiry(user._id(), Limits.ACCESS_EXPIRY_IN_SECONDS);
-        return ResponseEntity.ok(new CompleteBody(accessToken));
+        return new ResponseEntity<>(new AuthResponseBody(accessToken), HttpStatus.OK);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(
+            HttpServletResponse response,
+            HttpServletRequest request
+    ) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) return new ResponseEntity<>("no cookies sent to refresh", HttpStatus.FORBIDDEN);
+        String refreshToken = null;
+        for (Cookie cookie : cookies) {
+            if ("refresh-token".equals(cookie.getName())) {
+                refreshToken = cookie.getValue();
+            }
+        }
+        if (refreshToken == null) return new ResponseEntity<>("refresh token not found in cookies", HttpStatus.FORBIDDEN);
+
+        String userId = jwtService.extractToken(refreshToken);
+        if (userId == null) return new ResponseEntity<>("refresh token invalid", HttpStatus.FORBIDDEN);
+        if (userService.getAllUserInfo(userId) == null) return new ResponseEntity<>("user not in database", HttpStatus.FORBIDDEN);
+
+        String newRefreshToken = jwtService.createTokenBySubAndExpiry(userId, Limits.REFRESH_EXPIRY_IN_SECONDS);
+        Cookie cookie = createTokenCookie(newRefreshToken);
+        response.addCookie(cookie);
+
+        String newAccessToken = jwtService.createTokenBySubAndExpiry(userId, Limits.ACCESS_EXPIRY_IN_SECONDS);
+        return new ResponseEntity<>(new AuthResponseBody(newAccessToken), HttpStatus.OK);
     }
 }
