c9a9262a2a499e2310bdc131b4483b5cbaa8fdc9
Young Seo Kim <youngseokim7@gmail.com>
2025-08-11 14:21:29 +0900

    load all env variables on startup

--- src/main/java/com/JCC/LeAtcoderAPI/LeAtcoderApiApplication.java
+++ src/main/java/com/JCC/LeAtcoderAPI/LeAtcoderApiApplication.java
@@ -8,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeAtcoderApiApplication {
 
 	public static void main(String[] args) {
-		Dotenv.load();
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(LeAtcoderApiApplication.class, args);
 	}
 
